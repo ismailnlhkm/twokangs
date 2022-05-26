8a1980c8b5052034aa51e635c1d17a0cb115794c
ismailnlhkm <102459263+ismailnlhkm@users.noreply.github.com>
2022-05-27 05:00:15 +0700

    init

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const ProfileController = require('../controllers/ProfileController')
+const ProfileController = require('../controllers/profileController')
 const router = express.Router()
 
 router.get('/', ProfileController.profile)
